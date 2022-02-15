ca50a75e5403c0aad101630353345200b8fdd2fb
Stefan Pekez <stefanpekez5@gmail.com>
2022-02-15 23:24:29 +0100

    Initial commit. Made the game window

---
+++ core/src/com/arcanetower/game/ArcaneTower.java
@@ -0,0 +1,22 @@
+package com.arcanetower.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class ArcaneTower extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 1, 1, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
---
+++ desktop/src/com/arcanetower/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.arcanetower.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arcanetower.game.ArcaneTower;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		
+		new LwjglApplication(new ArcaneTower(), config);
+	}
+}
